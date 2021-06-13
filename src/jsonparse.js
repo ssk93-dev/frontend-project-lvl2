1923495efa103e2510651e2fd8a9c0515f10afec
Sergei Kalikin <sskalikin@gmail.com>
2021-06-13 23:21:17 +0500

    fix: test

--- src/jsonparse.js
+++ src/jsonparse.js
@@ -4,10 +4,8 @@ import path from 'path';
 
 const getPath = (filename) => path.resolve(process.cwd(), filename);
 const genDiff = (filepath1, filepath2) => {
-  const file1 = fs.readFileSync(getPath(filepath1), 'utf8');
-  const file2 = fs.readFileSync(getPath(filepath2), 'utf8');
-  const contentFromFile1 = JSON.parse(file1);
-  const contentFromFile2 = JSON.parse(file2);
+  const contentFromFile1 = JSON.parse(fs.readFileSync(getPath(filepath1), 'utf8'));
+  const contentFromFile2 = JSON.parse(fs.readFileSync(getPath(filepath2), 'utf8'));
   const keysFromFile1 = Object.keys(contentFromFile1);
   const keysFromFile2 = Object.keys(contentFromFile2);
   const sortedMergedKeys = _.sortBy(_.union(keysFromFile1, keysFromFile2));
@@ -17,9 +15,7 @@ const genDiff = (filepath1, filepath2) => {
         acc.push(`    ${item}: ${contentFromFile1[item]}`);
         return acc;
       }
-      acc.push(
-        `  - ${item}: ${contentFromFile1[item]}\n  + ${item}: ${contentFromFile2[item]}`,
-      );
+      acc.push(`  - ${item}: ${contentFromFile1[item]}\n  + ${item}: ${contentFromFile2[item]}`);
       return acc;
     }
     if (_.has(contentFromFile2, item)) {
