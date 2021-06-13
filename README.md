9b56c23d5fe4edabd936cb46511f2d1b25af893f
Sergei Kalikin <sskalikin@gmail.com>
2021-06-13 20:35:07 +0500

    add: fixtures, gendiff for json

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-### Hexlet tests and linter status:
+## Hexlet tests and linter status:
 [![Actions Status](https://github.com/ssk93-dev/frontend-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/ssk93-dev/frontend-project-lvl2/actions)
+
+## About
+Compares two configuration files and shows a difference.<br>
+### Example for 2 JSON files
+```sh
+{
+    "host": "hexlet.io",
+    "timeout": 50,
+    "proxy": "123.234.53.22",
+    "follow": false
+  }
+```
+```sh
+{
+    "timeout": 20,
+    "verbose": true,
+    "host": "hexlet.io"
+  }
+```
+[![asciicast](https://asciinema.org/a/ZYOUPs5v95Y8mwZOQIxE5lIH9.svg)](https://asciinema.org/a/ZYOUPs5v95Y8mwZOQIxE5lIH9)
---
+++ src/jsonparse.js
@@ -0,0 +1,34 @@
+import * as fs from 'fs';
+import _ from 'lodash';
+import path from 'path';
+
+const genDiff = (filepath1, filepath2) => {
+  const path1 = path.resolve(process.cwd(), filepath1);
+  const path2 = path.resolve(process.cwd(), filepath2);
+  if (fs.accessSync(path1, fs.constants.F_OK) === undefined
+  && fs.accessSync(path2, fs.constants.F_OK) === undefined) {
+    const contentFromFile1 = JSON.parse(fs.readFileSync(path1, 'utf8'));
+    const contentFromFile2 = JSON.parse(fs.readFileSync(path2, 'utf8'));
+    const mergedKeys = _.union(Object.keys(contentFromFile1), Object.keys(contentFromFile2));
+    const sortedMergedKeys = _.sortBy(mergedKeys);
+    const result = sortedMergedKeys.reduce((acc, item) => {
+      if (_.has(contentFromFile1, item) && _.has(contentFromFile2, item)) {
+        if (contentFromFile1[item] === contentFromFile2[item]) {
+          acc.push(`    ${item}: ${contentFromFile1[item]}`);
+          return acc;
+        }
+        acc.push(`  - ${item}: ${contentFromFile1[item]}\n  + ${item}: ${contentFromFile2[item]}`);
+        return acc;
+      }
+      if (_.has(contentFromFile2, item)) {
+        acc.push(`  + ${item}: ${contentFromFile2[item]}`);
+        return acc;
+      }
+      acc.push(`  - ${item}: ${contentFromFile1[item]}`);
+      return acc;
+    }, []);
+    return `{\n${result.join('\n')}\n}`;
+  }
+  return false;
+};
+export default genDiff;
