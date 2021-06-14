fe44a536c6355ed586a7ff927ccfd6858f5ff272
Sergei Kalikin <sskalikin@gmail.com>
2021-06-14 17:05:30 +0500

    fix: differece

--- src/jsonparse.js
+++ src/jsonparse.js
@@ -3,46 +3,65 @@ import _ from 'lodash';
 import path from 'path';
 
 const getPath = (filename) => path.resolve(process.cwd(), filename);
-const getContenet = (filename) => JSON.parse(fs.readFileSync(getPath(filename), 'utf8'));
+const getContent = (filename) => JSON.parse(fs.readFileSync(getPath(filename), 'utf8'));
 const getKeys = (object) => Object.keys(object);
-const addDifferences = (acc, key, content1, content2, condition) => {
-  switch (condition) {
-    case 'same':
-      acc.push(`    ${key}: ${content1[key]}`);
-      return acc;
-    case 'different':
-      acc.push(`  - ${key}: ${content1[key]}\n  + ${key}: ${content2[key]}`);
-      return acc;
-    case 'added':
-      acc.push(`  + ${key}: ${content2[key]}`);
-      return acc;
-    case 'removed':
-      acc.push(`  - ${key}: ${content1[key]}`);
-      return acc;
-    default:
-      return acc;
-  }
-};
-const getDiffereces = (keys, content1, content2) => {
-  const differences = keys.reduce((acc, item) => {
-    if (_.has(content1, item) && _.has(content2, item)) {
-      return (content1[item] === content2[item])
-        ? addDifferences(acc, item, content1, content2, 'same')
-        : addDifferences(acc, item, content1, content2, 'different');
+const formatDiff = (differences) => {
+  const formattedDiff = differences.map((item) => {
+    switch (item.type) {
+      case 'unchanged':
+        return `    ${item.name}: ${item.value}`;
+      case 'changed':
+        return `  - ${item.name}: ${item.value.oldValue}\n  + ${item.name}: ${item.value.newValue}`;
+      case 'added':
+        return `  + ${item.name}: ${item.value}`;
+      case 'removed':
+        return `  - ${item.name}: ${item.value}`;
+      default:
+        return '';
     }
-    return _.has(content2, item)
-      ? addDifferences(acc, item, content1, content2, 'added')
-      : addDifferences(acc, item, content1, content2, 'removed');
-  }, ['{']);
-  differences.push('}');
-  return differences.join('\n');
+  });
+  formattedDiff.unshift('{');
+  formattedDiff.push('}');
+  return formattedDiff.join('\n');
 };
-const genDiff = (filepath1, filepath2) => {
-  const contentFromFile1 = getContenet(filepath1);
-  const contentFromFile2 = getContenet(filepath2);
+const getDiffereces = (contentFromFile1, contentFromFile2) => {
   const keysFromFile1 = getKeys(contentFromFile1);
   const keysFromFile2 = getKeys(contentFromFile2);
-  const sortedMergedKeys = _.sortBy(_.union(keysFromFile1, keysFromFile2));
-  return getDiffereces(sortedMergedKeys, contentFromFile1, contentFromFile2);
+  const commonKeys = _.union(keysFromFile1, keysFromFile2);
+  const differences = commonKeys.map((item) => {
+    if (!_.has(contentFromFile1, item)) {
+      return {
+        name: item,
+        value: contentFromFile2[item],
+        type: 'added',
+      };
+    }
+    if (!_.has(contentFromFile2, item)) {
+      return {
+        name: item,
+        value: contentFromFile1[item],
+        type: 'removed',
+      };
+    }
+    if (contentFromFile1[item] === contentFromFile2[item]) {
+      return {
+        name: item,
+        value: contentFromFile1[item],
+        type: 'unchanged',
+      };
+    }
+    return {
+      name: item,
+      value: { oldValue: contentFromFile1[item], newValue: contentFromFile2[item] },
+      type: 'changed',
+    };
+  });
+  return _.sortBy(differences, 'name');
+};
+const genDiff = (filepath1, filepath2) => {
+  const contentFromFile1 = getContent(filepath1);
+  const contentFromFile2 = getContent(filepath2);
+  const differences = getDiffereces(contentFromFile1, contentFromFile2);
+  return formatDiff(differences);
 };
 export default genDiff;
