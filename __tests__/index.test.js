c4905e0d2ae387dd2bafdc3394712dd48b214020
Sergei Kalikin <sskalikin@gmail.com>
2021-06-20 19:44:37 +0500

    fix: tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,38 +7,19 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const filesToCompare = [
+  { file1: 'file1.json', file2: 'file2.json' },
+  { file1: 'file1.yaml', file2: 'file2.yml' },
+  { file1: 'file1.json', file2: 'file2.yml' },
+];
 
-test.each([
-  {
-    file1: 'file1.json', file2: 'file2.json', format: undefined, result: 'result_Stylish.txt',
-  },
-  {
-    file1: 'file1.yaml', file2: 'file2.yml', format: 'stylish', result: 'result_Stylish.txt',
-  },
-  {
-    file1: 'file1.json', file2: 'file2.yml', format: 'stylish', result: 'result_Stylish.txt',
-  },
-  {
-    file1: 'file1.json', file2: 'file2.json', format: 'plain', result: 'result_Plain.txt',
-  },
-  {
-    file1: 'file1.yaml', file2: 'file2.yml', format: 'plain', result: 'result_Plain.txt',
-  },
-  {
-    file1: 'file1.json', file2: 'file2.yml', format: 'plain', result: 'result_Plain.txt',
-  },
-  {
-    file1: 'file1.json', file2: 'file2.json', format: 'json', result: 'result_Json.txt',
-  },
-  {
-    file1: 'file1.yaml', file2: 'file2.yml', format: 'json', result: 'result_Json.txt',
-  },
-  {
-    file1: 'file1.json', file2: 'file2.yml', format: 'json', result: 'result_Json.txt',
-  },
-])('genDiff($file1, $file2), $format', ({
-  file1, file2, format, result,
-}) => {
-  expect(genDiff(getFixturePath(file1), getFixturePath(file2), format))
-    .toEqual(fs.readFileSync(getFixturePath(result), 'utf-8'));
+test.each(filesToCompare)('genDiff($file1, $file2)', ({ file1, file2 }) => {
+  expect(genDiff(getFixturePath(file1), getFixturePath(file2)))
+    .toEqual(fs.readFileSync(getFixturePath('result_Stylish.txt'), 'utf-8'));
+  expect(genDiff(getFixturePath(file1), getFixturePath(file2), 'stylish'))
+    .toEqual(fs.readFileSync(getFixturePath('result_Stylish.txt'), 'utf-8'));
+  expect(genDiff(getFixturePath(file1), getFixturePath(file2), 'plain'))
+    .toEqual(fs.readFileSync(getFixturePath('result_Plain.txt'), 'utf-8'));
+  expect(genDiff(getFixturePath(file1), getFixturePath(file2), 'json'))
+    .toEqual(fs.readFileSync(getFixturePath('result_Json.txt'), 'utf-8'));
 });
